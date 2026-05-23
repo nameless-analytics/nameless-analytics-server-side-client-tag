05a6d1af302fe763246e560ef5a4d8b5de6d973e
Tommaso Moretti <tommasomoretti88@gmail.com>
2026-05-23 19:01:43 +0200

    fix: update User-Agent string comparison to lowercase for streaming protocol validation

--- Nameless Analytics _ SS _ Client Tag.js
+++ Nameless Analytics _ SS _ Client Tag.js
@@ -174,7 +174,7 @@ if (getRequestPath() === endpoint) {
           return;
         }
 
-        if (event_origin === 'Streaming protocol' && request_user_agent !== 'Nameless Analytics - Streaming protocol') {
+        if (event_origin === 'Streaming protocol' && request_user_agent !== 'nameless analytics - streaming protocol') {
           message = '🔴 Invalid User-Agent header value. Request from bot';
           if (data.enable_logs) { log(message); }
           status_code = 403;
