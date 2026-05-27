c9a4f86e0b4afe6c47628c69939a1e59378c778a
Tommaso Moretti <tommasomoretti88@gmail.com>
2026-05-27 15:21:05 +0200

    feat: add session_landing_page_url and session_exit_page_url fields to session tracking data

--- Nameless Analytics _ SS _ Client Tag.js
+++ Nameless Analytics _ SS _ Client Tag.js
@@ -810,9 +810,11 @@ function send_to_firestore(event_data) {
             session_hostname: event_data.page_data.page_hostname,
             session_browser_name: event_data.event_data.browser_name,
             session_landing_page_category: event_data.page_data.page_category,
+            session_landing_page_url: event_data.page_data.page_url,
             session_landing_page_location: event_data.page_data.page_location,
             session_landing_page_title: event_data.page_data.page_title,
             session_exit_page_category: event_data.page_data.page_category,
+            session_exit_page_url: event_data.page_data.page_url,
             session_exit_page_location: event_data.page_data.page_location,
             session_exit_page_title: event_data.page_data.page_title,
             session_start_timestamp: (event_data.event_name === 'page_view') ? event_data.event_timestamp : null,
@@ -952,9 +954,11 @@ function send_to_firestore(event_data) {
             session_hostname: event_data.page_data.page_hostname,
             session_browser_name: event_data.event_data.browser_name,
             session_landing_page_category: (event_data.page_data.page_category) ? event_data.page_data.page_category : null,
+            session_landing_page_url: event_data.page_data.page_url,
             session_landing_page_location: event_data.page_data.page_location,
             session_landing_page_title: event_data.page_data.page_title,
             session_exit_page_category: (event_data.page_data.page_category) ? event_data.page_data.page_category : null,
+            session_exit_page_url: event_data.page_data.page_url,
             session_exit_page_location: event_data.page_data.page_location,
             session_exit_page_title: event_data.page_data.page_title,
             session_start_timestamp: (event_data.event_name === 'page_view') ? event_data.event_timestamp : null,
@@ -1023,9 +1027,11 @@ function send_to_firestore(event_data) {
             "session_hostname",
             "session_browser_name",
             "session_landing_page_category",
+            "session_landing_page_url",
             "session_landing_page_location",
             "session_landing_page_title",
             "session_exit_page_category",
+            "session_exit_page_url",
             "session_exit_page_location",
             "session_exit_page_title",
             "session_start_timestamp",
@@ -1046,6 +1052,7 @@ function send_to_firestore(event_data) {
 
           // Update session values in Firestore from current event data
           last_session.session_exit_page_category = (event_data.page_data.page_category) ? event_data.page_data.page_category : null;
+          last_session.session_exit_page_url = event_data.page_data.page_url;
           last_session.session_exit_page_location = event_data.page_data.page_location;
           last_session.session_exit_page_title = event_data.page_data.page_title;
           last_session.session_end_timestamp = event_data.event_timestamp;
